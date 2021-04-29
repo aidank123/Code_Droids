7c9622c04f5409c0e6cb032f9407e337a225218f
aidank123 <askelley@smcm.edu>
2021-04-29 17:08:19 -0400

    updates

--- src/rushbot_1/DeliveryDrone.java
+++ src/rushbot_1/DeliveryDrone.java
@@ -2,37 +2,12 @@ package rushbot_1;
 
 import battlecode.common.*;
 
+import java.util.ArrayList;
+
 public class DeliveryDrone extends RobotPlayer {
 
-    public static void runDeliveryDrone() throws GameActionException {
-
-        //step 1 set hq location
-        if(hq_location == null){
-            Communications.getHQLocation();
-        } else if(enemy_hq_location == null){
-            Communications.getEnemyHQLocation();
-        }
-
-        //CHECKS EVERY OTHER ROUND ON EVEN NUMBERS
-        if(rc.getRoundNum() % 2 == 0) {
-            Communications.receiveCommands();
-        }
-
-//        Team enemy = rc.getTeam().opponent();
-//        if (!rc.isCurrentlyHoldingUnit()) {
-//            // See if there are any enemy robots within capturing range
-//            RobotInfo[] robots = rc.senseNearbyRobots(GameConstants.DELIVERY_DRONE_PICKUP_RADIUS_SQUARED, enemy);
-//
-//            if (robots.length > 0) {
-//                // Pick up a first robot within range
-//                rc.pickUpUnit(robots[0].getID());
-//                System.out.println("I picked up " + robots[0].getID() + "!");
-//            }
-//        } else {
-//            // No close robots, so search for robots within sight radius
-//            tryMove(randomDirection());
-//        }
-
+    static void runDeliveryDrone() throws GameActionException {
+        ArrayList list = new ArrayList();
         MapLocation cowloc = null;
         Team enemy = rc.getTeam().opponent();
         if (!rc.isCurrentlyHoldingUnit()) {
@@ -40,23 +15,58 @@ public class DeliveryDrone extends RobotPlayer {
             RobotInfo[] robots = rc.senseNearbyRobots(GameConstants.DELIVERY_DRONE_PICKUP_RADIUS_SQUARED, enemy);
             if (robots.length > 0) {
                 for (RobotInfo robot : robots) {
+
                     //check all in radius and check for any cows
                     if (robot.type == RobotType.COW) {
+                        list.add(robot.getID());
                         //if robot in list is a cow then mark location
                         cowloc = robot.location;
+                        Direction directions_to_cow = rc.getLocation().directionTo(cowloc);
+                        tryMove(directions_to_cow);
+                        if (rc.getLocation().isAdjacentTo(cowloc)) {
+                            rc.pickUpUnit(robot.getID());
+                            break;
+                        }
+
                     }
                 }
-                //make direction to cow so possible to move there
-                Direction directions_to_cow = curr_loc.directionTo(cowloc);
-                tryMove(directions_to_cow);
-                // Pick up a first robot within range
-                rc.pickUpUnit(robots[0].getID());
-            }
-        } else {
-            // No close robots, so search for robots within sight radius
-            tryMove(randomDirection());
-        }
+                if (list.isEmpty()) {
+                    pathTo(randomDirection());
+                }
 
-        tryMove(randomDirection());
+            }
+
+        } else {
+            MapLocation nexttoenemyhq = enemy_hq_location.add(randomDirection());
+            Direction drone_to_HQ = rc.getLocation().directionTo(nexttoenemyhq);
+            tryMove(drone_to_HQ);
+            if (rc.getLocation().isAdjacentTo(enemy_hq_location) && rc.canDropUnit(Direction.EAST)) {
+                rc.dropUnit(Direction.EAST);
+            }
+
+        }
+        if (hq_location == null) {
+            Communications.getHQLocation();
+        } else {
+            for (Direction dir : directions) {
+
+                MapLocation defensivedrones = hq_location.add(dir);
+                Direction defensetoHQ = rc.getLocation().directionTo(defensivedrones);
+                if (rc.getLocation().isAdjacentTo(hq_location)) {
+                    break;
+                } else if (!rc.canMove(defensetoHQ)) {
+                    tryMove(randomDirection());
+
+                } else {
+                    tryMove(defensetoHQ);
+
+
+                }
+
+
+            }
+
+
+        }
     }
 }
